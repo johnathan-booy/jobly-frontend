9cb39f7855ed922f082e6b40f39b132ade2190e5
Johnathan Booy <johnny_music@icloud.com>
2023-02-02 07:42:44 -0800

    Basic Routes.js structure

---
+++ jobly-frontend/src/Routes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+
+function Routes() {
+	return (
+		<Switch>
+			<Route exact path="/">
+				<h1>Homepage</h1>
+			</Route>
+			<Route exact path="/companies">
+				<h1>All companies</h1>
+			</Route>
+			<Route path="/companies/:name">
+				<h1>One company</h1>
+			</Route>
+			<Route exact path="/jobs">
+				<h1>All jobs</h1>
+			</Route>
+			<Route exact path="/login">
+				<h1>Login</h1>
+			</Route>
+			<Route exact path="/signup">
+				<h1>Signup</h1>
+			</Route>
+			<Route exact path="/profile">
+				<h1>Edit profile</h1>
+			</Route>
+			<Redirect to="/" />
+		</Switch>
+	);
+}
+
+export default Routes;
