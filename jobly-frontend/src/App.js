7f0ea900191e3989fc91930d5c60f656b5f60cac
Johnathan Booy <johnny_music@icloud.com>
2023-02-02 07:16:38 -0800

    Cleared cra content

--- jobly-frontend/src/App.js
+++ jobly-frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>I am an h1</h1>
+			<h2>I am an h2</h2>
+			<h3>I am an h3</h3>
+			<h4>I am an h4</h4>
+			<h5>I am an h5</h5>
+			<h6>I am an h6</h6>
+			<p>I am a paragraph</p>
+			<b>I am bold text</b>
+			<i>I am italic text</i>
+			<u>I am underlined text</u>
+			<ol>
+				<li>I am an ordered list item</li>
+			</ol>
+			<ul>
+				<li>I am an unordered list item</li>
+			</ul>
+
+			<a href="#">I am a link</a>
+			<div>I am a div</div>
+			<span>I am a span</span>
+		</div>
+	);
 }
 
 export default App;
